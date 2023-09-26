859a9110bdc635507d2e355664aa37a5c81ef501
Ryan Lundy <rlundy1792@gmail.com>
2023-09-26 16:46:58 -0400

    Debugged code for local development

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,3 +1,4 @@
+/*
 // deployment config
 const pgp = require("pg-promise")();
 require("dotenv").config();
@@ -12,18 +13,19 @@ const db = pgp({
 });
 
 module.exports = db;
+*/
 
 // local dev config
-// const pgp = require("pg-promise")();
-// require("dotenv").config();
+const pgp = require("pg-promise")();
+require("dotenv").config();
 
-// const cn = {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER
-// };
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER
+};
 
-// const db = pgp(cn);
+const db = pgp(cn);
 
-// module.exports = db;
+module.exports = db;
--- queries/journals.js
+++ queries/journals.js
@@ -18,7 +18,7 @@ const getUnreadJournalsOfPatientByTherapist = async (tid, pid) => {
 // query to get one unread journal from one patient by therapist
 const getUnreadJournalOfPatientByTherapist = async (tid, pid, jid) => {
     try {
-        const unreadJournalOfPatientByTherapist = await db.one(
+        const unreadJournalOfPatientByTherapist = await db.oneOrNone(
             `SELECT p.id p_id, j.* FROM therapists t JOIN patients p ON (t.id = p.therapist_id) 
             JOIN journal_entries j ON (p.id=j.patient_id) 
             WHERE t.id=$1 and p.id=$2 AND j.id=$3 AND j.read = false 
@@ -33,11 +33,16 @@ const getUnreadJournalOfPatientByTherapist = async (tid, pid, jid) => {
 // query to update journal entry
 const updateJournal = async (jid, journal) => {
     try {
+        const query = `
+        UPDATE journal_entries 
+        SET patient_id=$1, entry_date=$2, journal_entry=$3, therapist_notes=$4, analysis_score=$5, ai_response=$6, read=$7
+        WHERE id=$8 
+        RETURNING *
+        `
+
         const updatedJournal = await db.one(
-            `UPDATE journal_entries 
-            SET patient_id=$1, entry_date=$2, journal_entry=$3, therapist_notes=$4, analysis_score=$5, read=$6
-            WHERE id=$7 RETURNING *`,
-            [journal.patient_id, journal.entry_date, journal.journal_entry, journal.therapist_notes, journal.analysis_score, journal.read, jid]
+            query,
+            [journal.patient_id, journal.entry_date, journal.journal_entry, journal.therapist_notes, journal.analysis_score, journal.ai_response, journal.read, jid]
         );
         return { updatedJournal };
     } catch (error) {
@@ -63,14 +68,14 @@ const createJournal = async (journal) => {
 };
 
 // query to get all journal entries
-// const getAllJournalsByTherapist = async () => {
-//     try {
-//         const allJournals = await db.any("SELECT * FROM journal_entries");
-//         return { allJournals };
-//     } catch (error) {
-//         return { error: error };
-//     }
-// };
+const getAllJournalsByTherapist = async () => {
+    try {
+        const allJournals = await db.any("SELECT * FROM journal_entries");
+        return { allJournals };
+    } catch (error) {
+        return { error: error };
+    }
+};
 
 // // query to get one journal entry
 // const getJournal = async (id) => {
@@ -97,8 +102,8 @@ module.exports = {
     getUnreadJournalsOfPatientByTherapist,
     getUnreadJournalOfPatientByTherapist,
     updateJournal,
-    createJournal
-    // getAllJournalsByTherapist,
+    createJournal,
+    getAllJournalsByTherapist
     // getJournal,
     // deleteJournal
 }
