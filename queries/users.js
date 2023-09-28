47e464bdd6d41c96ef72a5a3ac968c0f8d6f18f3
christian orlando <christianorlando@pursuit.org>
2023-09-28 16:01:46 -0700

    logIn with userId continue with Th_id or P_id

--- queries/users.js
+++ queries/users.js
@@ -3,13 +3,25 @@ const db = require("../db/dbConfig");
 // query to get one user
 const getUserByID = async (id) => {
     try {
-        const user = await db.one("SELECT * FROM users WHERE id= $1", id);
+        const user = await db.one("SELECT t.id t_id,p.id p_id,u.id u_id,u.role role FROM therapists t right join users u on (t.user_id=u.id) left join patients p on (u.id = p.user_id) WHERE u.id= $1", id);
         return { user };
     } catch (error) {
         return { error: error };
     }
 };
 
+const getTherapistByUserID = async (id) => {
+    try {
+        const user = await db.one("SELECT t.id FROM therapists t join users u  on (t.user_id=u.id) WHERE u.id= $1", id);
+        return { user };
+    } catch (error) {
+        return { error: error };
+    }
+};
+
+
+
 module.exports = {
-    getUserByID
+    getUserByID,
+    getTherapistByUserID,
 }
