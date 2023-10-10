c0f58faca0e192cf4f73a97838b8ed3e6d40de86
Ryan Lundy <rlundy1792@gmail.com>
2023-10-10 18:01:02 -0400

    Updated dbConfig.js for deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,31 +1,31 @@
 
-// deployment config
-const pgp = require("pg-promise")();
-require("dotenv").config();
-
-const connectionString = process.env.ELEPHANTSQL_URL;
-
-const db = pgp({
-    connectionString: connectionString,
-    ssl: {
-        rejectUnauthorized: false  // Used for ElephantSQL as it requires SSL
-    }
-});
-
-module.exports = db;
-
-
-// // local dev config
+// // deployment config
 // const pgp = require("pg-promise")();
 // require("dotenv").config();
 
-// const cn = {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER
-// };
+// const connectionString = process.env.ELEPHANTSQL_URL;
 
-// const db = pgp(cn);
+// const db = pgp({
+//     connectionString: connectionString,
+//     ssl: {
+//         rejectUnauthorized: false  // Used for ElephantSQL as it requires SSL
+//     }
+// });
 
 // module.exports = db;
+
+
+// local dev config
+const pgp = require("pg-promise")();
+require("dotenv").config();
+
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER
+};
+
+const db = pgp(cn);
+
+module.exports = db;
