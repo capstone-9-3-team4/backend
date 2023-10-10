3c9862f32f26f13efe767abc0c48fc2291e65b93
Ryan Lundy <rlundy1792@gmail.com>
2023-10-09 20:49:31 -0400

    Removed 'role' alias from getUserByID query

--- queries/users.js
+++ queries/users.js
@@ -3,7 +3,7 @@ const db = require("../db/dbConfig");
 // query to get one user
 const getUserByID = async (id) => {
     try {
-        const user = await db.one("SELECT t.id t_id,p.id p_id,u.id u_id,u.role role FROM therapists t right join users u on (t.user_id=u.id) left join patients p on (u.id = p.user_id) WHERE u.id= $1", id);
+        const user = await db.one("SELECT t.id t_id,p.id p_id,u.id u_id,u.role FROM therapists t right join users u on (t.user_id=u.id) left join patients p on (u.id = p.user_id) WHERE u.id= $1", id);
         return { user };
     } catch (error) {
         return { error: error };
